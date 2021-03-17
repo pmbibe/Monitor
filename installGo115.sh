1fb9c2e61b98677fb3d5cad553a59381472f2919
pmbibe <53257014+pmbibe@users.noreply.github.com>
2021-03-17 15:27:06 +0700

    Update installGo115.sh

--- installGo115.sh
+++ installGo115.sh
@@ -15,7 +15,7 @@ function installGo {
     wget https://dl.google.com/go/go1.15.linux-amd64.tar.gz
     tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz && rm -rf go1.15.linux-amd64.tar.gz
     echo """export PATH=$PATH:/usr/local/go/bin""" >> /etc/profile
-    . /etc/profile
+    source /etc/profile
     checkGo
 }
 checkGo
